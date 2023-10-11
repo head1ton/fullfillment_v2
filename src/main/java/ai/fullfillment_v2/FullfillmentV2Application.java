71a78b5d6e6933d07fed521a3d400f6f70d5361a
head1ton <head1ton@gmail.com>
2023-10-11 20:46:33 +0900

    Initial commit

---
+++ src/main/java/ai/fullfillment_v2/FullfillmentV2Application.java
@@ -0,0 +1,13 @@
+package ai.fullfillment_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullfillmentV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullfillmentV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ai/fullfillment_v2/FullfillmentV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.fullfillment_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullfillmentV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
