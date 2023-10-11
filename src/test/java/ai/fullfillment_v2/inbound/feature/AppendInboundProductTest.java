b20dab27b97b3dfcc51b17703be411a2670aabd0
head1ton <head1ton@gmail.com>
2023-10-12 00:36:53 +0900

    2. create inner class : AppendInboundProduct

--- src/test/java/ai/fullfillment_v2/inbound/feature/AppendInboundProductTest.java
+++ src/test/java/ai/fullfillment_v2/inbound/feature/AppendInboundProductTest.java
@@ -1,14 +1,24 @@
 package ai.fullfillment_v2.inbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AppendInboundProductTest {
 
+    private AppendInboundProduct appendInboundProduct;
+
+    @BeforeEach
+    void setUp() {
+        appendInboundProduct = new AppendInboundProduct();
+    }
+
     @Test
     @DisplayName("입고 상품을 추가.")
     public void appendInboundProduct() {
-
     }
 
+    private class AppendInboundProduct {
+
+    }
 }
