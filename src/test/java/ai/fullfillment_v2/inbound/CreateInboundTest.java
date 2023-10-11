46c58d2adbe8b21d7faf32759918f56b86e940c5
head1ton <head1ton@gmail.com>
2023-10-11 21:20:19 +0900

    2. create test code

--- src/test/java/ai/fullfillment_v2/inbound/CreateInboundTest.java
+++ src/test/java/ai/fullfillment_v2/inbound/CreateInboundTest.java
@@ -1,5 +1,13 @@
 package ai.fullfillment_v2.inbound;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class CreateInboundTest {
 
+    @Test
+    @DisplayName("입고 생성")
+    public void createInbound() {
+
+    }
 }
