c15c3dbc8a2d23f27feaa443a146dfe53a24c2bc
head1ton <head1ton@gmail.com>
2023-10-12 00:36:02 +0900

    1. test case : AppendInboundProductTest

---
+++ src/test/java/ai/fullfillment_v2/inbound/feature/AppendInboundProductTest.java
@@ -0,0 +1,14 @@
+package ai.fullfillment_v2.inbound.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppendInboundProductTest {
+
+    @Test
+    @DisplayName("입고 상품을 추가.")
+    public void appendInboundProduct() {
+
+    }
+
+}
