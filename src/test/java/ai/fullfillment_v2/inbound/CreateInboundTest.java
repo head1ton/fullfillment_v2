0346ff6d4f4080a31e908ca016a951fe86a0d568
head1ton <head1ton@gmail.com>
2023-10-11 21:21:16 +0900

    3. create inner class : CreateInbound

--- src/test/java/ai/fullfillment_v2/inbound/CreateInboundTest.java
+++ src/test/java/ai/fullfillment_v2/inbound/CreateInboundTest.java
@@ -1,13 +1,25 @@
 package ai.fullfillment_v2.inbound;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CreateInboundTest {
 
+    private CreateInbound createInbound;
+
+    @BeforeEach
+    void setUp() {
+        createInbound = new CreateInbound();
+    }
+
     @Test
     @DisplayName("입고 생성")
     public void createInbound() {
+
+    }
+
+    private class CreateInbound {
 
     }
 }
