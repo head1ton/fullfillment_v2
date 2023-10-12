5cf520b89b6d36a3a66fbd5b8199c6d096d23590
head1ton <head1ton@gmail.com>
2023-10-12 11:20:06 +0900

    3. create method : request

--- src/test/java/ai/fullfillment_v2/inbound/feature/UpdateInboundProductTest.java
+++ src/test/java/ai/fullfillment_v2/inbound/feature/UpdateInboundProductTest.java
@@ -1,17 +1,28 @@
 package ai.fullfillment_v2.inbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UpdateInboundProductTest {
 
+    private UpdateInboundProduct updateInboundProduct;
+
+    @BeforeEach
+    void setUp() {
+        updateInboundProduct = new UpdateInboundProduct();
+    }
+
     @Test
     @DisplayName("입고 상품 수정.")
     public void updateInboundProduct() {
-        UpdateInboundProduct updateInboundProduct = new UpdateInboundProduct();
+        updateInboundProduct.request();
     }
 
     private class UpdateInboundProduct {
 
+        public void request() {
+            throw new UnsupportedOperationException("Unsupported request");
+        }
     }
 }
