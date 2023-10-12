d1d611b151d7b2c9b6bec292891f8714516afa4b
head1ton <head1ton@gmail.com>
2023-10-12 11:18:35 +0900

    1. test case : UpdateInboundProductTest

---
+++ src/test/java/ai/fullfillment_v2/inbound/feature/UpdateInboundProductTest.java
@@ -0,0 +1,14 @@
+package ai.fullfillment_v2.inbound.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UpdateInboundProductTest {
+
+    @Test
+    @DisplayName("입고 상품 수정.")
+    public void updateInboundProduct() {
+
+    }
+
+}
